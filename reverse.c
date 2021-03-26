9c6ab9c2406da47a3ed627a3d06b5c48a489cd10
niiloliimatainen <niilo.liimatainen@student.lut.fi>
2021-03-26 17:02:17 +0200

    Version 1.0

--- reverse.c
+++ reverse.c
@@ -4,45 +4,37 @@
 
 #define MAXLEN 1024
 
-struct lines
-{
+struct lines {
 	char *string;
 	struct lines *prev;
 	struct lines *next;
 };
 
-int read_lines(struct lines **line, FILE *stream)
-{
+
+/*Function to read lines from the input stream to the doubly linked list*/
+int read_lines(struct lines **line, FILE *stream) {
 	char tmp[MAXLEN];
 	struct lines *lp;
 
-	while (fgets(tmp, MAXLEN, stream) != NULL)
-	{
+	while (fgets(tmp, MAXLEN, stream) != NULL) {
 
-		if (*line == NULL)
-		{
-			if ((*line = lp = malloc(sizeof(struct lines))) == NULL)
-			{
+		if (*line == NULL) {
+			if ((*line = lp = malloc(sizeof(struct lines))) == NULL) {
 				fprintf(stderr, "ERROR: could not allocate memory\n");
 				return -1;
 			}
 			lp->prev = lp->next = NULL;
-		}
-		else
-		{
-			if ((lp->next = malloc(sizeof(struct lines))) == NULL)
-			{
+		
+		} else {
+			if ((lp->next = malloc(sizeof(struct lines))) == NULL) {
 				fprintf(stderr, "ERROR: could not allocate memory\n");
 				return -1;
 			}
-
-			/* Here you should insert the lines that link the list correctly */
 			lp->next->prev = lp;
 			lp = lp->next;
 		}
 
-		if ((lp->string = malloc(strlen(tmp) + 1)) == NULL)
-		{
+		if ((lp->string = malloc(strlen(tmp) + 1)) == NULL) {
 			fprintf(stderr, "ERROR: could not allocate memory\n");
 			return -1;
 		}
@@ -53,37 +45,32 @@ int read_lines(struct lines **line, FILE *stream)
 	return 0;
 }
 
-void print_lines(struct lines *line, FILE *stream)
-{
+
+/*Function to write lines from linked list in reverse order to the output stream*/
+void write_lines(struct lines *line, FILE *stream) {
 	struct lines *lp;
-	/* Here you should implement a loop that prints the lines in reverse order. 	*/
-	/* First you need to follow the links from the first node to the last node.	*/
 	lp = line;
-	printf("Here is the stdin in reverse direction\n");
-	/*Muutetaan jokaisen solmun prev ja next osoittimet keskenään*/
-	while (lp != NULL)
-	{
+	/*Reversing a doubly linked list*/
+	while (lp != NULL) {
 		line = lp->prev;
 		lp->prev = lp->next;
 		lp->next = line;
 		lp = lp->prev;
 	}
-
+	/*Writing lines from the reversed list to the output stream*/
 	lp = line->prev;
-	while (lp != NULL)
-	{
-		printf("%s", lp->string);
+	while (lp != NULL) {
+		fprintf(stream, "%s", lp->string);
 		lp = lp->next;
 	}
-	printf("\n");
 }
 
-void delete_lines(struct lines *line)
-{
+
+/*Function to free the allocated doubly linked list*/
+void delete_lines(struct lines *line) {
 	struct lines *lp;
 	lp = line;
-	while (lp != NULL)
-	{
+	while (lp != NULL) {
 		line = lp->next;
 		free(lp->string);
 		free(lp);
@@ -91,59 +78,61 @@ void delete_lines(struct lines *line)
 	}
 }
 
-int main(int argc, char **argv)
-{ /*IF argc = 1 read from stdin and write to stdout*/
+
+int main(int argc, char **argv) { 
+	/*IF argc = 1 read from stdin and write to stdout*/
 	/*IF argc = 2 -> user gives inputfile which prints to screen <stdout>*/
 	/*IF argc = 3 -> same thing as 2, but writes to provided file instead of stdout*/
 	/*IF ELSE be mad and crash*/
 	struct lines *line = NULL;
 
-	if (argc == 1)
-	{
-		if (read_lines(&line, stdin) == -1)
+	if (argc == 1) {
+		printf("Input your text: \n");
+		if (read_lines(&line, stdin) == -1) {
 			exit(1);
-		print_lines(line, stdout);
-	}
-	else if (argc == 2)
-	{
+		}
+		printf("\nData from stdin in reversed order:\n");
+		write_lines(line, stdout);
+
+	} else if (argc == 2) {
 		FILE *input;
 
-		if ((input = fopen(argv[1], "r")) == NULL)
-		{
-			printf("Error: cannot open file '%s'", argv[1]);
+		if ((input = fopen(argv[1], "r")) == NULL) {
+			fprintf(stderr, "Error: cannot open file '%s'\n", argv[1]);
 			exit(1);
 		}
+		printf("Data from file '%s' in reversed order:\n", argv[1]);
+		if (read_lines(&line, input) == -1) {
+			exit(1);
+		}
+		write_lines(line, stdout);
+		fclose(input);
 
-		if (read_lines(&line, input) == -1)
-		{
+	} else if (argc == 3) {
+		if (strcmp(argv[1], argv[2]) == 0) {
+			fprintf(stderr, "Input and output file must differ\n");
 			exit(1);
 		}
-		print_lines(line, stdout);
-	}
-	else if (argc == 3)
-	{
 		FILE *input;
 		FILE *output;
-		if ((input = fopen(argv[1], "r")) == NULL)
-		{
-			printf("Error: cannot open file '%s'", argv[1]);
+		if ((input = fopen(argv[1], "r")) == NULL) {
+			fprintf(stderr, "Error: cannot open file '%s'\n", argv[1]);
 			exit(1);
 		}
 
-		if (read_lines(&line, input) == -1)
-		{
+		if (read_lines(&line, input) == -1) {
 			exit(1);
 		}
-		if ((output = fopen(argv[2], "w")) == NULL)
-		{
-			printf("Error: cannot open file '%s'", argv[2]);
+		if ((output = fopen(argv[2], "w")) == NULL) {
+			fprintf(stderr, "Error: cannot open file '%s'\n", argv[1]);
 			exit(1);
 		}
+		write_lines(line, output);
+		printf("Data from file '%s' writed to the file '%s' in reversed order\n", argv[1], argv[2]);
+		fclose(input);
+		fclose(output);
 
-		print_lines(line, output);
-	}
-	else
-	{
+	} else {
 		printf("Wrong amount of arguments!\n Usage: reverse <optional inputfile.txt> <optional outputfile.txt>");
 		exit(1);
 	}
